604bcc9267bf15e3ee533815535fb7116a387b98
IonRod <rrod76@gmail.com>
2017-12-16 18:12:05 +0300

    Implemented onlyMember

--- contracts/TaskVoting.sol
+++ contracts/TaskVoting.sol
@@ -5,43 +5,57 @@ contract TaskVoting {
     uint public taskQuantity = 0;
     mapping(uint => Task) arrayOfTasks;
     mapping(address =>  mapping(uint => bool)) arrayOfVotingStates;
-    struct Task{
+    struct Task {
         uint32 condition;/*0 - предложена, 1 - одобрена, 2 - в работе, 3 - готова*/
         uint positiveVotesNumber;
         string Description;
     }
-    address[] memberAddresses = [
-         0x108d3d2ac5926977c69da2e6342bdc99de38e3f1, 0x38f491cb707daf3d9bfdcc60f18ad139fe87eabb
-        ];
+    mapping(address => bool) memberAddresses;
+    uint numToWin;
     
-/********************ФУНКЦИИ************************/
     
-    function createTask(string _description) public{
-    arrayOfTasks[taskQuantity] = Task({
-            condition: 0,
-            positiveVotesNumber: 0,
-            Description: _description
-        });
-    
-    taskQuantity++;
+    modifier onlyMember() {
+      require(memberAddresses[msg.sender] == true);
+      _;
     }
     
-    function taskInformation(uint _taskNumber) public view returns(uint, uint, string){
+/********************ФУНКЦИИ************************/
+    function TaskVoting(address[] members) {
+      numToWin = members.length/2;
+      for(uint i = 0; i < members.length; ++i) {
+        memberAddresses[members[i]] = true;
+      }
+    }
+    
+    function createTask(string _description) onlyMember public {
+      arrayOfTasks[taskQuantity] = Task({
+              condition: 0,
+              positiveVotesNumber: 0,
+              Description: _description
+          });
+      
+      taskQuantity++;
+    }
+    
+    function taskInformation(uint _taskNumber) public view returns(uint, uint, string) {
         return(arrayOfTasks[_taskNumber].condition, arrayOfTasks[_taskNumber].positiveVotesNumber, arrayOfTasks[_taskNumber].Description);
     }
     
-    function Vote(uint _taskNumber,bool _decision) public {
+    function Vote(uint _taskNumber, bool _decision) onlyMember public {
         
-        if (_decision == true && arrayOfVotingStates[msg.sender][_taskNumber] == false){
-        arrayOfTasks[_taskNumber].positiveVotesNumber ++;
+        if (_decision == true && arrayOfVotingStates[msg.sender][_taskNumber] == false) {
+          arrayOfTasks[_taskNumber].positiveVotesNumber++;
         }
         arrayOfVotingStates[msg.sender][_taskNumber] = true;
         
-        if (arrayOfTasks[_taskNumber].positiveVotesNumber>memberAddresses.length/2){
+        if (arrayOfTasks[_taskNumber].positiveVotesNumber > numToWin) {
             arrayOfTasks[_taskNumber].condition = 1 ;
         }
     }
     
+    function checkIfMember(address member) public view returns (bool) {
+      return memberAddresses[member];
+    }
 
     
 }
